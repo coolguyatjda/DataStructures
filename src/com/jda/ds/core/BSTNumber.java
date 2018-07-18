bf0ee468ef47f4548a637d7760203b0a2e02055b
Sanket Rathi <Sanket.Rathi@jda.com>
2018-07-18 12:33:33 +0000

    bst

--- src/com/jda/ds/core/BSTNumber.java
+++ src/com/jda/ds/core/BSTNumber.java
@@ -1,10 +1,28 @@
 package com.jda.ds.core;
 
+import java.util.Scanner;
+
 public class BSTNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static	int catalan(int n) {
+      int res = 0;
+       
+      // Base case
+      if (n <= 1) {
+          return 1;
+      }
+      for (int i = 0; i < n; i++) {
+          res += catalan(i) * catalan(n - i - 1);
+      }
+      return res;
+  }
 
-	}
+  public static void main(String[] args) {
+     
+      Scanner in = new Scanner(System.in);
+      int i = in.nextInt();
+     System.out.print(catalan(i));
+     
+  }
 
 }
